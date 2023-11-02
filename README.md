8bf3fb10f38d550361835c2757aa168fcfc09a89
Sam Pink <samuel.pink@outlook.com>
2023-11-02 08:41:42 +0000

    added comments

--- README.md
+++ README.md
@@ -1,14 +1,34 @@
 # Stock Sentiment Analysis App
 
-This app performs sentiment analysis on a given stock using recent news articles and stock data.
+The Stock Sentiment Analysis App is an innovative tool designed to gauge the market's mood on any given stock by scrutinizing the latest news and stock data. It utilizes the cognitive processing power of OpenAI's GPT-4 to deliver insights into stock sentiment, making it an intriguing asset for traders and investors alike.
 
-## Features
-- Fetches real-time stock data.
-- Gathers recent news articles related to the stock.
-- Performs sentiment analysis using OpenAI GPT-4.
+## Concept
+At its core, the app marries financial data with the subtleties of news analysis, offering an edge in a data-driven trading environment. Its ability to parse through volumes of data and provide a sentiment score is what makes it captivating.
 
-## Setup
-1. Clone the repository.
-2. Install dependencies:
-   ```bash
-   pip install -r requirements.txt
+## Journey to Deployment
+
+### Initial Steps
+- **Development**: Started with crafting the app in Python, ensuring it ran flawlessly locally.
+- **Testing**: Rigorously tested functionalities to guarantee accurate sentiment analysis.
+
+### Containerization with Docker
+- **Docker Image**: Created a Docker image to containerize the application, allowing for consistent deployment across various environments.
+- **Local Testing**: Ensured the Docker image was stable and operational on a local machine.
+
+### Orchestration and Scaling
+- **Docker-Compose**: Composed a Docker-Compose file to simulate multi-container deployment, preparing for a microservices architecture.
+- **Kubernetes**: Authored a Kubernetes manifest to manage the app in a more scalable and resilient fashion.
+
+### Cloud Deployment
+- **Google Cloud Run**: Chose Google Cloud Run for its seamless integration and ease of use, pushing the Docker image to the cloud.
+- **Live URL**: The application was deployed successfully and is now accessible [here](https://stockapp-r7agec3nya-nw.a.run.app).
+
+## How It Works
+1. Fetches real-time stock data and recent related news articles.
+2. Uses OpenAI GPT-4 for nuanced sentiment analysis.
+3. Provides an interface to interact with and visualize the sentiment data.
+
+## Final Thoughts
+This journey from a simple Python script to a full-fledged cloud application encapsulates the modern development lifecycle, demonstrating the power of cloud-native technologies coupled with AI.
+
+To explore the app's functionalities, visit the documentation [here](https://stockapp-r7agec3nya-nw.a.run.app/docs#/).
--- app.py
+++ app.py
@@ -1,50 +1,80 @@
 import yfinance as yf
-import numpy as np
 import requests
-from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi import FastAPI, HTTPException, status, Path
 import uvicorn
 import logging
 import os
-
-
 import openai
 import instructor
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
+# Apply patches provided by 'instructor' to enhance introspection capabilities
 instructor.patch()
 
-# Setup logging
+# Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Environment Variables for API Keys
+# Environment Variables for API Keys (fallback to placeholder if not set)
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "your-openai-api-key")
 SERPAPI_KEY = os.getenv("SERPAPI_KEY", "your-serpapi-key")
 
-app = FastAPI()
+# Initialize FastAPI app instance
+app = FastAPI(
+    title="Stock Sentiment Analysis App",
+    description="This app analyzes the sentiment of a given stock ticker by evaluating recent news articles and historical stock data, providing insights on whether to buy or sell.",
+    version="1.0.0",
+)
 
-# Cache for S&P 500 data
+
+# Initialize cache for S&P 500 data
 sp500_data = None
 
 
-# create a pydantic response model
-class response_model(BaseModel):
-    buy_or_sell: str
-    analysis: str
-    news_sentiment: str
-    current_price_usd: float
-    predicted_price_usd_next_week: float
-    predicted_price_usd_next_month: float
+# Define Pydantic model for structured responses
+class ResponseModel(BaseModel):
+    buy_or_sell: str = Field(
+        ..., description="Recommendation to buy or sell the stock", example="Buy"
+    )
+    analysis: str = Field(
+        ...,
+        description="Detailed sentiment analysis",
+        example="The sentiment is positive based on recent news articles.",
+    )
+    news_sentiment: str = Field(
+        ...,
+        description="Sentiment of the news articles",
+        example="The sentiment is positive based on recent news articles.",
+    )
+    current_price_usd: float = Field(
+        ...,
+        description="Current price of the stock in USD",
+        example=100.0,
+    )
+    predicted_price_usd_next_week: float = Field(
+        ...,
+        description="Predicted price of the stock in USD for next week",
+        example=110.0,
+    )
+    predicted_price_usd_next_month: float = Field(
+        ...,
+        description="Predicted price of the stock in USD for next month",
+        example=120.0,
+    )
 
 
+# Event handler for application startup
 @app.on_event("startup")
 async def load_sp500_data():
+    """Load and cache S&P 500 data at startup."""
     global sp500_data
     sp500 = yf.Ticker("^GSPC")
-    sp500_data = sp500.history(period="2w")  # Cache the S&P 500 data at startup
+    sp500_data = sp500.history(period="4w")
 
 
+# Utility functions
 def get_news(ticker: str):
+    """Fetch news articles for a given stock ticker."""
     params = {"q": f"{ticker} stock news", "tbm": "nws", "api_key": SERPAPI_KEY}
     response = requests.get("https://serpapi.com/search", params=params)
     if response.status_code != 200:
@@ -56,6 +86,7 @@ def get_news(ticker: str):
 
 
 def generate_sentiment_analysis_prompt(stock_data, news_data, ticker):
+    """Generate a prompt for sentiment analysis using stock and news data."""
     news_articles = "\n".join(
         [
             f"Title: {item['title']} - Snippet: {item['snippet']}"
@@ -64,7 +95,7 @@ def generate_sentiment_analysis_prompt(stock_data, news_data, ticker):
     )
     return f"""
     Analyze the sentiment of the stock {ticker} based on the recent news articles and stock data.
-    ive included the snp 500 data for the last 2 weeks as a baseline.
+    I've included the S&P 500 data for the last month as a baseline.
 
     News Articles:
     {news_articles}
@@ -72,17 +103,49 @@ def generate_sentiment_analysis_prompt(stock_data, news_data, ticker):
     Stock Data:
     {stock_data.to_string()}
     
-    snp 500 data:
+    S&P 500 Data:
     {sp500_data.to_string()}
     """
 
 
-@app.get("/sentiment/{ticker}")
-async def analyze_sentiment(ticker: str):
+# API endpoints
+@app.get(
+    "/sentiment/{ticker}",
+    summary="Analyze Stock Sentiment",
+    description="Retrieve sentiment analysis and trading recommendation for a given stock ticker based on news articles and stock data.",
+    response_model=ResponseModel,
+    responses={
+        200: {
+            "description": "A successful response with analysis and recommendations",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "buy_or_sell": "Buy",
+                        "analysis": "Positive sentiment observed with a potential increase in stock price",
+                        "current_price": 150.42,
+                        "predicted_price_usd_next_week": 155.50,
+                        "predicted_price_usd_next_month": 160.75,
+                    }
+                }
+            },
+        },
+        404: {
+            "description": "Stock ticker not found",
+            "content": {
+                "application/json": {
+                    "example": {"detail": "Stock ticker 'XYZ' not found"}
+                }
+            },
+        },
+    },
+)
+async def analyze_sentiment(ticker: str = Path(..., example="AAPL")):
+    """Endpoint for sentiment analysis of a given stock ticker."""
     stock = yf.Ticker(ticker)
-    stock_data = stock.history(period="2w")
+    stock_data = stock.history(period="4w")
     news_data = get_news(ticker)
 
+    # Handle cases where the stock data is not found
     if stock_data.empty:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Stock not found"
@@ -90,19 +153,19 @@ async def analyze_sentiment(ticker: str):
 
     prompt = generate_sentiment_analysis_prompt(stock_data, news_data, ticker)
 
+    # Set API key for OpenAI service
     openai.api_key = OPENAI_API_KEY
-    
-    response: response_model = openai.ChatCompletion.create(
+
+    # Create a chat completion using OpenAI's API
+    response: ResponseModel = openai.ChatCompletion.create(
         model="gpt-4",
-        messages=[
-            {"role": "user", "content": prompt},
-        ],
-        response_model=response_model,
+        messages=[{"role": "user", "content": prompt}],
+        response_model=ResponseModel,
     )
 
     return response
 
 
+# Entry point for running the application
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
-
